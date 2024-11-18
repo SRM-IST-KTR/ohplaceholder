198865738bd1e3a57cf9c0dbb1be9edbc1eaff02
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2024-11-18 09:19:24 +0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customPurpleStart: '#5d1a66',
+        customPurpleEnd: '#330051',
       },
     },
   },
