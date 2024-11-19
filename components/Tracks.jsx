5bc337229e61df37c2c02db8f768db77e07f025a
keshitrivedi <keshitrivedi@gmail.com>
2024-11-19 19:35:25 +0530

    thisismylaststraw

---
+++ components/Tracks.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Tracks = () => {
+    return (
+        <>
+            <div className="mt-[250px]">
+                <div class="text-center text-[119px] font-bold text-[#FCF961] mb-[80px] [text-shadow:_0_7px_10px_rgb(255_255_255_/_90%)]">TRACKS</div>
+                    <div class="grid lg:grid-cols-[303px_303px_303px] md:grid-cols-[303px_303px] sm:grid-cols-[303px] gap-[113px] justify-center mb-[80px]">
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[30px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center font-semibold absolute bottom-[30px]">AI/ML</div>
+                    </div>
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[30px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center font-semibold absolute bottom-[30px]">Healthcare</div>
+                    </div>
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[15px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center leading-[45px] font-semibold absolute bottom-[30px]">Open Innovation</div>
+                    </div>
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[30px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center font-semibold mt-[30px] absolute bottom-[30px]">TBA</div>
+                    </div>
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[30px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center font-semibold mt-[30px] absolute bottom-[30px]">TBA</div>
+                    </div>
+
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                        <div class="mt-[30px]"></div>
+                        <div class="text-[42px] text-[#EBE100] text-center font-semibold mt-[30px] absolute bottom-[30px]">TBA</div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Tracks;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Shared/Navbar/Navbar";
+import Tracks from "@/components/Tracks.jsx";
 import Footer from "@/components/Shared/Footer/Footer";
 import Scroll from "@/components/Shared/Scroll";
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Tracks />
       <div className="z-500">
         <Scroll />
       </div>
