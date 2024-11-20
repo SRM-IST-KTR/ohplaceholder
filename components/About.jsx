a6068decb61ad3056b5191f021fcb6a8d5bf36c9
Sameer Yadav <ysameer0026@gmail.com>
2024-11-20 22:06:17 +0530

    corrected errors

--- components/About.jsx
+++ components/About.jsx
@@ -1,63 +1,87 @@
-import React from 'react';
-import grid1 from '../public/grid1.png';
-import grid2 from '../public/grid2.png';
-import man from '../public/aboutman.png';
-import Image from 'next/image';
+import React from 'react'
+import Image from "next/image";
+import localFont from "next/font/local";
+
+const geistSans = localFont({
+    src: "../public/fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
+});
+
+const geistMono = localFont({
+    src: "../public/fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
+});
 
 const About = () => {
-  return (
-    <>
-    <div className="min-h-screen bg-gradient-radial from-[#4C0058] to-[#1C0C1F] relative">
-        
-        <div className="absolute inset-0 z-10">
-          <Image 
-            src={grid1} 
-            alt="Background Grid 1" 
-            layout="fill" 
-            objectFit="cover" 
-            className="opacity-60" 
-          />
-        </div>
-
-        
-        <div className="absolute inset-0 z-20 flex justify-center items-center">
-          <Image 
-            src={grid1} 
-            alt="Background Grid 1" 
-            layout="fill" 
-            objectFit="cover" 
-            className="opacity-60 -ml-[50px]" 
-          />
-        </div>
-        
-
-        <div className="relative z-30">
-          <p className='text-4xl sm:text-6xl md:text-7xl text-[#FCF961] font-bold text-center pt-24 sm:pt-32'>
-            ABOUT THE EVENT
-          </p>
-
-          
-          <div className='sm:ml-10 md:ml-[100px]  xl:ml-[440px] pt-6 sm:pt-8 md:pt-10'>
-            <div className='bg-[#7B008F] rounded-2xl w-[90%] sm:w-[500px] md:w-[600px] lg:w-[700px] lg:-ml-[20px]  mx-auto'>
-              <p className='text-white p-6 sm:p-8 md:p-10 font-semibold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
-                OSSome Hacks is the new chapter of OSSmosis organized by the GitHub Community SRM. It is an open-source hackathon, where participants will brainstorm to leverage existing technologies; innovate and improve the existing systems and fix the loopholes for betterment.
-              </p>
+    return (
+        <div className={`${geistSans.variable} ${geistMono.variable} relative`}>
+            {/* About section */}
+            <div
+                className="about w-[1036px] h-[156px] flex items-center justify-center bg-black-100 opacity-100"
+                style={{
+                    marginLeft: '250px',
+                    top: '116px',
+                    left: '12px',
+                    position: 'relative',
+                }}
+            >
+                <h1
+                    style={{
+                        fontFamily: 'Inter',
+                        fontSize: '100px',
+                        fontWeight: 700,
+                        lineHeight: '128.28px',
+                        textAlign: 'left',
+                        color: '#FCF961',
+                    }}
+                >
+                    ABOUT THE EVENT
+                </h1>
             </div>
-          </div>
 
-         
-          <div className=''>
-            <Image 
-              src={man} 
-              alt="Man Image"
-              className='absolute w-[200px] -mt-[610px] ml-[264px] hidden lg:block '
-              
-            />
-          </div>
+            {/* Main content section */}
+            <div
+                className="aboutMain w-[1100px] h-[450px] bg-[#7B008F] opacity-100 rounded-[55px] relative flex items-center"
+                style={{
+                    top: '190px',
+                    left: '270px',
+                    position: 'relative',
+                    padding: '40px',
+                }}
+            >
+                <Image
+                    src="/man.png"
+                    alt="Description of the image"
+                    width={330}
+                    height={620}
+                    className="rounded-[20px] absolute"
+                    style={{
+                        top: '-260px',
+                        left: '-250px',
+                    }}
+                />
+
+                <p
+                    className="para"
+                    style={{
+                        fontFamily: 'Inter',
+                        fontWeight: 700,
+                        fontSize: '35px', // Increased font size for more coverage
+                        lineHeight: '50px', // Increased line height for balanced spacing
+                        letterSpacing: '3.5%',
+                        color: 'white',
+                        textAlign: 'justify-left', // Justify text to cover the box width
+                        marginLeft: '180px',
+                        maxWidth: '600px',
+                    }}
+                >
+                    OSSome Hacks is the new chapter of OSSmosis organized by the GitHub Community SRM. It is an open-source hackathon, where participants will brainstorm to leverage existing technologies; innovate and improve the existing systems and fix the loopholes for betterment.
+                </p>
+            </div>
         </div>
-      </div>
-    </>
-  )
+    )
 }
 
 export default About
