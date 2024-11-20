308d326cc1bbd6bd4caedae00724577faa3190b2
Valli81106 <sreevalliiscute1234@gmail.com>
2024-11-20 17:01:58 +0530

    Tracks

--- components/Tracks.jsx
+++ components/Tracks.jsx
@@ -8,42 +8,48 @@ const Tracks = () => {
                 <div className="text-center text-[119px] font-bold text-[#FCF961] mb-[80px] [text-shadow:_0_7px_10px_rgb(255_255_255_/_90%)]">TRACKS</div>
                     <div className="grid lg:grid-cols-[303px_303px_303px] md:grid-cols-[303px_303px] sm:grid-cols-[303px] gap-[113px] justify-center mb-[80px]">
 
-                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                 transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[30px]">
                         <img src="../../AIML_icon.png" class="w-[168px] h-[177px]"></img>
                         </div>
                         <div className="text-[42px] text-[#EBE100] text-center font-semibold absolute bottom-[30px]">AI/ML</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                    transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[30px]">
                             <img src="../../healthcare_icon.png" class="w-[152px] h-[152px]"></img>
                         </div>
                         <div className="text-[42px] text-[#EBE100] text-center font-semibold absolute bottom-[30px]">Healthcare</div>
                     </div>
 
-                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[15px]">
                             <img src="../../open_innovation_icon.png" className="w-[201px] h-[201px]"></img>
                         </div>
                         <div className="text-[42px] text-[#EBE100] text-center leading-[45px] font-semibold absolute bottom-[30px]">Open Innovation</div>
                     </div>
 
-                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[30px]">
                         <img src="..\..\megaphone_icon.png" className="w-[176px] h-[176px]"></img>
                         </div>
                         <div className="text-[42px] text-[#EBE100] text-center font-semibold mt-[30px] absolute bottom-[30px]">TBA</div>
                     </div>
 
-                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div class="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[30px]">
                         <img src="..\..\megaphone_icon.png" className="w-[176px] h-[176px]"></img>
                         </div>
                         <div className="text-[42px] text-[#EBE100] text-center font-semibold mt-[30px] absolute bottom-[30px]">TBA</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col items-center relative transition ease-in-out delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)]
+                                    transition-transform duration-300 ease-in-out transform hover:scale-105">
                         <div className="mt-[30px]">
                         <img src="..\..\megaphone_icon.png" className="w-[176px] h-[176px]"></img>
                         </div>
