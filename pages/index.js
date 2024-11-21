8f4ce2680161c4c9e5f2a056d809323b7d4da891
Sameer Yadav <ysameer0026@gmail.com>
2024-11-21 17:05:34 +0530

    fixed file structure of about us

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import About from "@/components/About";
-import Aboutus from "@/components/About/About";
+
 
 export default function Home() {
   return (
     <>
     <About />
-    <Aboutus/>
+    
     </>
   );
 }
