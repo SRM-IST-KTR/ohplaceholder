c29d4e4ce7291c380937983fba9a901e665cc007
keshitrivedi <keshitrivedi@gmail.com>
2024-11-29 18:10:26 +0530

    cards center on hover

--- components/Tracks.jsx
+++ components/Tracks.jsx
@@ -10,7 +10,7 @@ const Tracks = () => {
                 </div>
                     <div className="grid grid-cols-[303px] gap-[113px] justify-center align-middle items-center">
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-height ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../AIML_icon.png" class="w-[152px] h-[152px]"></img>
@@ -21,7 +21,7 @@ const Tracks = () => {
                         <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../healthcare_icon.png" class="w-[152px] h-[152px]"></img>
@@ -32,7 +32,7 @@ const Tracks = () => {
                         <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[30px] w-[303px] flex justify-center">
                                 <img src="../../open_innovation_icon.png" class="w-[152px] h-[152px]"></img>
@@ -44,7 +44,7 @@ const Tracks = () => {
                         <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../megaphone_icon.png" class="w-[152px] h-[152px]"></img>
@@ -55,7 +55,7 @@ const Tracks = () => {
                         <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../megaphone_icon.png" class="w-[152px] h-[152px]"></img>
@@ -66,7 +66,7 @@ const Tracks = () => {
                         <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                     </div>
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../megaphone_icon.png" class="w-[152px] h-[152px]"></img>
