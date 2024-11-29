d63b6e586f071c7ef332fe1f75949a52c40edc6f
keshitrivedi <keshitrivedi@gmail.com>
2024-11-29 17:43:14 +0530

    tracks almost done

--- components/Tracks.jsx
+++ components/Tracks.jsx
@@ -5,10 +5,12 @@ const Tracks = () => {
         <>
             <div className="mt-[30px] flex flex-col">
                 
-                <div className="text-center align-middle justify-center lg:text-[119px] md:text-[100px] text-[70px] font-bold mb-[50px] text-[#FCF961] [text-shadow:0_7px_10px_rgb(255_255_255/_90%)]">TRACKS</div>
+                <div className="text-center align-middle justify-center lg:text-[119px] md:text-[100px] text-[70px] font-bold mb-[50px] text-[#FCF961] [text-shadow:0_7px_10px_rgb(255_255_255/_90%)]">
+                    TRACKS
+                </div>
                     <div className="grid grid-cols-[303px] gap-[113px] justify-center align-middle items-center">
 
-                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300">
+                    <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-height ease-in-out duration-300">
                         <div className="flex flex-col">
                             <div className="mt-[50px] w-[303px] flex justify-center">
                                 <img src="../../AIML_icon.png" class="w-[152px] h-[152px]"></img>
