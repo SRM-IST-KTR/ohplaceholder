652c15c71ef79a549f29aa59ee3b57903ffe716c
Valli81106 <sreevalliiscute1234@gmail.com>
2024-12-07 19:58:14 +0530

    Added Content

--- components/Tracks.jsx
+++ components/Tracks.jsx
@@ -8,7 +8,7 @@ const Tracks = () => {
                 <div className="text-center align-middle justify-center lg:text-[119px] md:text-[100px] text-[70px] font-bold mb-[50px] text-[#FCF961] [text-shadow:0_7px_10px_rgb(255_255_255/_90%)]">
                     TRACKS
                 </div>
-                    <div className="grid grid-cols-[303px] gap-[113px] justify-center align-middle items-center">
+                    <div className="grid grid-cols-[303px] gap-[60px] justify-center align-middle items-center">
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
                         <div className="flex flex-col">
@@ -17,8 +17,20 @@ const Tracks = () => {
                             </div>
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">AI/ML</div>
                         </div>
+                          
+                        <div>
+                            <div className="  pt-[30px] text-[30px] font-bold leading-tight text-white" > Shaping the Future with Intelligence
+
+                            </div>
+                            <div className="p-[20px] pt-[8px] font-medium text-lightgrey ">Step into the world of Artificial Intelligence and Machine Learning to
+                                                    create solutions that revolutionize industries. Build intelligent
+                                                    automation systems, recommendation engines, or AI-driven analytics
+                                                    that solve real-world problems. This track is your chance to harness the
+                                                    power of AI/ML and lead the charge in shaping the future with
+                                                    transformative technologies.</div>
+                        </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        
                     </div>
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
@@ -28,8 +40,22 @@ const Tracks = () => {
                             </div>
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">Healthcare</div>
                         </div>
+
+                        <div>
+                            
+                        </div>
+                        <div>
+                             <div className="  pt-[30px] text-[30px] font-bold leading-tight text-white" > Innovating for a Healthier Tomorrow
+
+                            </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className="p-[20px]">Think about the difference you could make in someone’s life by
+                                                reimagining healthcare through technology. In this track, we’re looking
+                                                for ideas that can solve real-world health challenges. Whether it’s
+                                                creating tools for better patient care, improving hospital processes, or</div>
+                        </div>
+
+                    
                     </div>
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
@@ -40,8 +66,22 @@ const Tracks = () => {
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[-15px]">Open</div>
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">Innovation</div>
                         </div>
+
+                        <div>
+
+                            <div className="  pt-[30px] text-[30px] font-bold leading-tight text-white" > Your Ideas, Your Rules
+
+                            </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className="p-[20px]">This is your space to dream big and build boldly. The Open Innovation
+                                                    Track gives you complete freedom to create solutions for the problems
+                                                    you care about the most. Whether it’s tackling climate change,
+                                                    improving education, or reinventing entertainment, the sky's the limit
+                                                    for this track, let your creativity run wild. Whatever you’ve been waiting
+                                                    to build, now’s the time to make it happen.</div>
+                        </div>
+
+                        
                     </div>
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
@@ -52,7 +92,7 @@ const Tracks = () => {
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">TBA</div>
                         </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className="p-[10px] text-[40px] font-bold leading-tight text-white">To be announced soon !!</div>
                     </div>
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
@@ -63,7 +103,7 @@ const Tracks = () => {
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">TBA</div>
                         </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className="p-[10px] text-[40px] font-bold leading-tight text-white">To be announced soon !!</div>
                     </div>
 
                     <div className="bg-[#AB7A57] h-[318px] rounded-[20px] shadow-[0_15px_30px_-15px_rgba(0,0,0,0.7)] flex flex-col lg:flex-row items-center relative delay-100 hover:shadow-[0_0_30px_0_rgba(255,255,255,1)] overflow-hidden hover:h-[700px] lg:hover:w-[700px] lg:hover:h-[318px] text-center transition-all ease-in-out duration-300 lg:hover:translate-x-[-175px]">
@@ -74,7 +114,7 @@ const Tracks = () => {
                             <div className="text-[42px] text-[#EBE100] text-center font-semibold mb-[30px]">TBA</div>
                         </div>
                         
-                        <div className="p-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                        <div className="p-[10px]  text-[40px] font-bold leading-tight text-white">To be announced soon !!</div>
                     </div>
 
                     
