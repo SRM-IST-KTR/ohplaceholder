236e356097c31396d47ae9018da8071dedcceacd
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-12-09 21:18:32 +0530

    custom bg

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import About from "@/components/About";
+import Grid1 from "../public/grid1.png";
+import Grid2 from "../public/grid2.png";
 
 
 export default function Home() {
   return (
-    <>
-    <About />
-    
-    </>
+    <div className="h-screen">
+      <div className="absolute inset-0 bg-custom-gradient -z-50"></div>
+      <div className="absolute inset-0 bg-[url('../public/grid1.png')] bg-cover bg-center opacity-80"></div>
+      <div className="absolute inset-0 bg-[url('../public/grid2.png')] bg-cover bg-center opacity-60 mix-blend-multiply"></div>
+      <div className="relative">
+        <About />
+      </div>
+    </div>
   );
 }
