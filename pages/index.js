67bf953735980378ce280ec887f7fd3b4aa670f9
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-12-10 22:51:33 +0530

    bg fix

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ import Grid2 from "../public/grid2.png";
 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div className="absolute inset-0 bg-custom-gradient -z-50"></div>
+    <div className="">
       <div className="absolute inset-0 bg-[url('../public/grid1.png')] bg-cover bg-center opacity-80"></div>
       <div className="absolute inset-0 bg-[url('../public/grid2.png')] bg-cover bg-center opacity-60 mix-blend-multiply"></div>
       <div className="relative">
